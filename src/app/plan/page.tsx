9822417707f2d6d5cae90344cba0aae56ea2b000
Kim Handong <khdkkhdd@gmail.com>
2023-11-17 17:49:59 +0900

    Update page.tsx

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -118,7 +118,7 @@ export default function Plan(props: PlanpageProps) {
 
   const onconfirmhandler = async () => {
     try {
-      const response = await fetch("http://43.202.89.97:52458/main", {
+      const response = await fetch("https://43.202.89.97:52678/main", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
